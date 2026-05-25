72b64195f50d547be6e7777dc11d7fdf7ec20245
TadejPogago <antonfroilan@outlook.com>
2026-05-26 01:53:09 +0800

    Modified SqkDb.cs

--- DataHelper/SqlDb.cs
+++ DataHelper/SqlDb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,28 +10,132 @@ namespace DataHelper
 {
     public class SqlDb
     {
-        static string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SalesInventoryDb;Integrated Security=True;Connect Timeout=30;";
+        static string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MasterFile;Integrated Security=True;Connect Timeout=30;";
 
-        public static bool ChangePassword(string username, string currentPass, string newPass)
+        public static int ExecuteNonQuery
+        (
+            string storedProcedure,
+            SqlParameter[] parameters
+        )
         {
             using (SqlConnection con = new SqlConnection(connString))
             {
+                using (SqlCommand cmd =
+                    new SqlCommand(storedProcedure, con))
+                {
+                    cmd.CommandType =
+                        CommandType.StoredProcedure;
+
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    con.Open();
+
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static object ExecuteScalar
+        (
+            string storedProcedure,
+            SqlParameter[] parameters = null
+        )
+        {
+            using (SqlConnection con =
+                new SqlConnection(connString))
+            {
+                using (SqlCommand cmd =
+                    new SqlCommand(storedProcedure, con))
+                {
+                    cmd.CommandType =
+                        CommandType.StoredProcedure;
+
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    con.Open();
+
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static DataTable ExecuteDataTable
+        (
+            string storedProcedure,
+            SqlParameter[] parameters = null
+        )
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection con =
+                new SqlConnection(connString))
+            {
+                using (SqlCommand cmd =
+                    new SqlCommand(storedProcedure, con))
+                {
+                    cmd.CommandType =
+                        CommandType.StoredProcedure;
+
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    using (SqlDataAdapter da =
+                        new SqlDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+            }
+
+            return dt;
+        }
+
+        public static bool ChangePassword
+        (
+            string username,
+            string currentPass,
+            string newPass
+        )
+        {
+            using (SqlConnection con =
+                new SqlConnection(connString))
+            {
                 con.Open();
 
-                // 1. check current password
-                string checkQuery = "SELECT COUNT(*) FROM Users WHERE Username=@u AND Password=@p";
-                SqlCommand checkCmd = new SqlCommand(checkQuery, con);
+                string checkQuery =
+                    @"SELECT COUNT(*)
+                      FROM Users
+                      WHERE Username=@u
+                      AND PasswordHash=@p";
+
+                SqlCommand checkCmd =
+                    new SqlCommand(checkQuery, con);
+
                 checkCmd.Parameters.AddWithValue("@u", username);
                 checkCmd.Parameters.AddWithValue("@p", currentPass);
 
-                int exists = (int)checkCmd.ExecuteScalar();
+                int exists =
+                    Convert.ToInt32(checkCmd.ExecuteScalar());
 
                 if (exists == 0)
                     return false;
 
-                // 2. update password
-                string updateQuery = "UPDATE Users SET Password=@np WHERE Username=@u";
-                SqlCommand updateCmd = new SqlCommand(updateQuery, con);
+                string updateQuery =
+                    @"UPDATE Users
+                      SET PasswordHash=@np
+                      WHERE Username=@u";
+
+                SqlCommand updateCmd =
+                    new SqlCommand(updateQuery, con);
+
                 updateCmd.Parameters.AddWithValue("@u", username);
                 updateCmd.Parameters.AddWithValue("@np", newPass);
 
