e28d9675b2ab9b369d107b000d5254ed31efe906
TadejPogago <antonfroilan@outlook.com>
2026-05-25 22:45:12 +0800

    Class Library - added connString

---
+++ DataHelper/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHelper
+{
+    public class DataAccess
+    {
+        static string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SalesInventoryDb;Integrated Security=True;Connect Timeout=30;";
+    }
+}
